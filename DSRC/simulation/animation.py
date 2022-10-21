c8e4f1bbe4de0a2cac0f98d11b1860fc66d1405d
connorfuhrman <connorfuhrman@outlook.com>
2022-10-20 23:27:33 -0700

    Tested kinematics, waypoint following, and docking with Ray and added MP4 save.

--- DSRC/simulation/animation.py
+++ DSRC/simulation/animation.py
@@ -5,6 +5,12 @@ from matplotlib.animation import FuncAnimation
 from itertools import groupby
 import numpy as np
 from DSRC.simulation import SimulationHistory
+from tqdm import tqdm
+
+class _animation_tqdm(tqdm):
+    """Provides `update_to(n)` which uses `tqdm.update(delta_n)`."""
+    def update_to(self, curr_frame, total_frame):
+        return self.update(total_frame - curr_frame) 
 
 
 def _all_equal(iterable):
@@ -25,7 +31,7 @@ def _update_plot(frame_num, datagetter, axs):
                     line.set(alpha=0.0)
             ax.set_title(f"Simulation {metadata['id']}\nat iteration {frame_num}")
         else:
-            for line in lines:
+            for line in lines.values():
                 line.set(alpha=0.0)
             ax.set_title("Simulation concluded")
 
@@ -43,7 +49,7 @@ def _get_plot_layout(nplots: int) -> tuple[int, int]:
 
 def entrypoint(sim_history: list[SimulationHistory]):
     """Animate the results of a simulation."""
-    fig = plt.figure()
+    fig = plt.figure(figsize=(15, 10))
     ax_map = dict()
     max_max_iters = 0
     nplot_rows, nplot_cols = _get_plot_layout(len(sim_history))
@@ -71,10 +77,10 @@ def entrypoint(sim_history: list[SimulationHistory]):
             metadata = None
         return data['lines'], history, metadata
 
-    ani = FuncAnimation(fig,                        # noqa F481: I know this is unused
-                        _update_plot,
-                        max_max_iters,
-                        fargs=(getdata, list(ax_map.keys()),),
-                        interval=10)
-
-    plt.show()
+    with _animation_tqdm(total=max_max_iters, desc="Animation MP4 Save") as t:
+        ani = FuncAnimation(fig,                        # noqa F481: I know this is unused
+                            _update_plot,
+                            max_max_iters+1,
+                            fargs=(getdata, list(ax_map.keys()),),
+                            interval=10)
+        ani.save("./simulation_animation.mp4", progress_callback = t.update_to)
