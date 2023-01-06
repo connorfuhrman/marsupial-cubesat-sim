d9861c207aac9b72827200b0323f31cfe4614194
Connor Fuhrman <connorfuhrman@email.arizona.edu>
2023-01-05 18:07:27 -0700

    Updates to training process

--- DSRC/experiments/training/bennu_particle_ejection_return.py
+++ DSRC/experiments/training/bennu_particle_ejection_return.py
@@ -30,28 +30,40 @@ def on_generation(ga_instance: pygad.GA):  # noqa D
     global save_dir
     global summary_writer
     global stop_fitness
+    global max_fitness
+
+    max_last_gen_fitness = max(ga_instance.last_generation_fitness)
+    if max_fitness is None or max_fitness < max_last_gen_fitness:
+        max_fitness = max_last_gen_fitness
+    min_last_gen_fitness = min(ga_instance.last_generation_fitness)
+    avg_last_gen_fitness = np.average(ga_instance.last_generation_fitness)
     
     # assert ga_instance.generations_completed == len(ga_instance.best_solutions_fitness)
-    max_overall = max(ga_instance.best_solutions_fitness)
     N = ga_instance.generations_completed
     print(f"Generation             = {N}")
-    print(f"Generation Max Fitness = {ga_instance.best_solutions_fitness[-1]}")
-    print(f"Overall Max Fitness    = {max_overall}")
+    print(f"Generation Max Fitness = {max_last_gen_fitness}")
+    print(f"Generation Min Fitness = {min_last_gen_fitness}")
+    print(f"Generation Avg Fitness = {avg_last_gen_fitness}")
+    print(f"Overall Max Fitness    = {max_fitness}")
     print("="*45)
 
     summary_writer.add_scalar("Generation", N, N)
-    summary_writer.add_scalar("Fitness/Max Overall", max_overall, N)
-    summary_writer.add_scalar("Fitness/Generation Max", max(ga_instance.last_generation_fitness), N)
-    summary_writer.add_scalar("Fitness/Generation Min", min(ga_instance.last_generation_fitness), N)
-    summary_writer.add_scalar("Fitness/Generation Avg", np.average(ga_instance.last_generation_fitness), N)
+    summary_writer.add_scalar("Fitness/Max Overall", max_fitness, N)
+    summary_writer.add_scalar("Fitness/Generation Max", max_last_gen_fitness, N)
+    summary_writer.add_scalar("Fitness/Generation Min", min_last_gen_fitness, N)
+    summary_writer.add_scalar("Fitness/Generation Avg", avg_last_gen_fitness, N)
     summary_writer.add_histogram("Fitness/Generation", ga_instance.last_generation_fitness, ga_instance.generations_completed)
     summary_writer.flush()
+
+    with open(f"{save_dir}/fitness-values.csv", "a") as f:
+        vals = ",".join(map(str, ga_instance.last_generation_fitness)) + "\n"
+        f.write(vals)
     
     if (ngens := ga_instance.generations_completed) % 5 == 0 or ngens == 1:
         ga_instance.save(f"{save_dir}/ga-generation-{ngens}")
 
-    if max_overall > stop_fitness:
-        print(f"Signaling to stop since fitness reached {stop_fitness} at generation {ga_instance.generations_completed}")
+    if max_fitness > stop_fitness:
+        print(f"Signaling to stop since fitness reached {stop_fitness} at generation {N}")
         return "stop"
 
         
@@ -66,7 +78,7 @@ class Trainer:
     def __init__(self, config,
                  num_solutions: int,
                  num_opt_procs: int,
-                 initial_population = None,
+                 initial_model = None,
                  num_experiments_per_fitness: int=1,
                  num_proc_per_fitness: int=1,
                  checkpoint_fname: str=None):
@@ -85,13 +97,16 @@ class Trainer:
         self.num_experiments_per_fitness = num_experiments_per_fitness
         self.num_proc_per_fitness = num_proc_per_fitness
         self.num_opt_procs = num_opt_procs
-        self.initial_population = initial_population
+        self.initial_model = initial_model
 
         print(f"Training with population size of {num_solutions}")
+        if self.initial_model is not None:
+            print("This training run was seeded with existing model weights")
         print(f"Fitness is averaged out of {self.num_experiments_per_fitness} runs")
         print(f"Fitness is calculated using {self.num_proc_per_fitness} procs")
         print(f"Optimizer uses {os.cpu_count() if (n := self.num_opt_procs) is None else n} procs")
-        print(f"Number of available processor cores is {os.cpu_count()}\n")
+        print(f"Number of available processor cores is {os.cpu_count()}")
+        print("="*45)
 
         # self.device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
         self.device = 'cpu'
@@ -114,7 +129,7 @@ class Trainer:
                 "console": {
                     "class": "logging.StreamHandler",
                     "formatter": "standard",
-                    "level": "INFO",
+                    "level": "CRITICAL",
                     "stream": sys.stdout,
                 },
                 # "file": {
@@ -127,7 +142,7 @@ class Trainer:
             },
             "loggers": {
                 logger_name: {
-                    "level": "WARN",
+                    "level": "CRITICAL",
                     "handlers": ["console"], #["console", "file"],
                     "propagate": False,
                 },
@@ -147,7 +162,7 @@ class Trainer:
         config = self.experiment_config.copy()
         config['model'] = model
 
-        fitness = [self._run_single_experiment(config)
+        fitness = [self._run_single_experiment(config.copy())
                    for _ in range(self.num_experiments_per_fitness)]
 
         # if self.num_opt_procs == 1:
@@ -167,7 +182,20 @@ class Trainer:
         return avg_fitness
 
     def _run_single_experiment(self, config):
-        # print("Starting experiment")
+        # Chose a random location for the mothership
+        mship_pos = np.random.default_rng().uniform(low=-500, high=500, size=(3,))
+        # Chose a random Bennu position some distance away
+        theta = np.random.default_rng().uniform(low=0, high=2.0*np.pi)
+        phi = np.random.default_rng().uniform(low=0, high=np.pi)
+        bennu_pos = np.array([dist_from_bennu * np.cos(theta) * np.sin(phi),
+                              dist_from_bennu * np.sin(theta) * np.sin(phi),
+                              dist_from_bennu * np.cos(phi)], dtype=float) + mship_pos
+
+        config['bennu_pos'] = bennu_pos
+        config['simulation_config']['mothership_config'][0]['initial_position'] = mship_pos
+
+        assert np.abs(np.linalg.norm(bennu_pos - mship_pos) - dist_from_bennu) <= 0.5  # Sanity check the distance from the mothership
+
         experiment = BennuParticleReturn(config, self.logger)
 
         try:
@@ -191,9 +219,12 @@ class Trainer:
             p_sample_value_recovered = 0.0
         p_invalid_docking_cmds = experiment.num_invalid_docking_commands/experiment.total_num_actions
         assert_btwn(p_invalid_docking_cmds, 0.0, 1.0)
-        
-        global_fitness = (0.65 * p_sample_value_recovered + 0.35 * p_cubesats_recovered) * (1.0 - p_invalid_docking_cmds)
-        assert_btwn(global_fitness, 0.0, 1.0)
+
+        if p_sample_value_recovered == 0.0 and p_cubesats_recovered == 0.0:
+            global_fitness = -1.0
+        else:
+            global_fitness = (0.65 * p_sample_value_recovered + 0.35 * p_cubesats_recovered) * (1.0 - p_invalid_docking_cmds)
+            assert_btwn(global_fitness, 0.0, 1.0)
 
         fitness = (0.9 * global_fitness) + (0.1 * experiment.final_episode_reward)
         assert_btwn(fitness, -1.0, 1.0)
@@ -201,11 +232,10 @@ class Trainer:
         return fitness
 
     def _new_ga_instance(self, num_solutions):
-        if self.initial_population is None:
-            model_ga = torchga.TorchGA(Model(), num_solutions)
-            initial_population = model_ga.population_weights  # Initial population of network weights
+        if self.initial_model is None:
+            initial_population = torchga.TorchGA(Model(), num_solutions).population_weights
         else:
-            initial_population = self.initial_population
+            initial_population = torchga.TorchGA(self.initial_model, num_solutions).population_weights
         
         num_generations = 1500  # Number of generations
         num_parents_mating = num_solutions  # Number of solutions to be selected as parents in the mating pool.
@@ -213,9 +243,6 @@ class Trainer:
         crossover_type = "single_point"  # Type of the crossover operator.
         mutation_type = "random"  # Type of the mutation operator.
         mutation_percent_genes = 25  # Percentage of genes to mutate.
-        # This parameter has no action if the parameter mutation_num_genes exists.
-        keep_parents = max(2, round(num_solutions/4))  # Number of parents to keep in the next population.
-        # -1 means keep all parents and 0 means keep nothing.
 
         parallel = ["process", n] if (n := self.num_opt_procs) is None or n > 1 else None
 
@@ -227,9 +254,10 @@ class Trainer:
                                     crossover_type=crossover_type,
                                     mutation_type=mutation_type,
                                     mutation_percent_genes=mutation_percent_genes,
-                                    keep_parents=keep_parents,
                                     on_generation=on_generation,
-                                    parallel_processing=parallel)
+                                    parallel_processing=parallel,
+                                    allow_duplicate_genes=False,
+                                    keep_elitism=round(num_solutions/4))
 
     def run(self):
         """Run the training session.
@@ -242,12 +270,12 @@ class Trainer:
         self.ga_instance.save(f"{save_dir}/ga_results-final")
         #self.ga_instance.plot_result(title="Bennu Sample Return Fitness", linewidth=4)
 
-        # model = Model()
+        model = Model()
         sol, fitness, _ = self.ga_instance.best_solution()
-        # weight_dict = torchga.model_weights_as_dict(model, sol)
-        # model.load_state_dict(weight_dict)
+        weight_dict = torchga.model_weights_as_dict(model, sol)
+        model.load_state_dict(weight_dict)
         # torch.save(model.state_dict(), f"/{save_dir}/bennu_particle_return_model_weights.pytorch_model")
-        return sol, fitness
+        return model, fitness
 
 
 if __name__ == '__main__':
@@ -260,13 +288,13 @@ if __name__ == '__main__':
                         default=os.cpu_count())
     parser.add_argument("--num_opt_procs",
                         type=int,
-                        default=None)  # PyGAD uses None as default
+                        default=os.cpu_count())
     parser.add_argument("--num_experiments_per_fitness",
                         type=int,
                         default=1)
-    parser.add_argument("--num_procs_per_fitness",
-                         type=int,
-                         default=1)
+    # parser.add_argument("--num_procs_per_fitness",
+    #                      type=int,
+    #                      default=1)
     parser.add_argument("--save_dir",
                         type=str,
                         default=None)
@@ -310,27 +338,32 @@ if __name__ == '__main__':
         }
     }
 
-    num_samples = [3, 5, 10, 15, 20, 50]
-    initial_pop = None
+    dist_from_bennu = 50
+    num_samples = [3, 4, 5, 10, 15, 20, 50]
+    starting_model = None
     for ns in num_samples:
-        experiment_config['max_num_samples'] = ns
-        trainer = Trainer(config=experiment_config,
-                          num_solutions=args.num_solutions,
-                          num_opt_procs=args.num_opt_procs,
-                          num_experiments_per_fitness=args.num_experiments_per_fitness,
-                          num_proc_per_fitness=args.num_procs_per_fitness)
-                          # checkpoint_fname=args.checkpoint_fname)
-        # Global Access #
-        save_dir = pathlib.Path(args.save_dir) / f"{ns}-samples"
-        summary_writer = SummaryWriter(save_dir)
-        stop_fitness = 0.9
-        ##############
-    
-        save_dir.mkdir(parents=True, exist_ok=True)
-    
         print("=" * 45)
         print(f"Training starting with {ns} samples ....")
         print("=" * 45)
+
+        experiment_config['max_num_samples'] = ns
+        trainer = Trainer(config=experiment_config,
+                          initial_model=starting_model,
+                          num_solutions=args.num_solutions,
+                          num_opt_procs=args.num_opt_procs,
+                          num_experiments_per_fitness=args.num_experiments_per_fitness,
+                          num_proc_per_fitness=1)
+                          # checkpoint_fname=args.checkpoint_fname)
+        #################
+        # Global Access #
+        save_dir = pathlib.Path(args.save_dir) / f"{ns}-samples"
+        summary_writer = SummaryWriter(save_dir)
+        max_fitness = None
+        stop_fitness = -0.85 #0.9
+        ################
+    
+        save_dir.mkdir(parents=True, exist_ok=True)
+
         # try:
         #     initial_pop, last_fitness = trainer.run()
         # except KeyboardInterrupt:
@@ -340,7 +373,7 @@ if __name__ == '__main__':
         #     extype, value, tb = sys.exc_info()
         #     traceback.print_exc()
         #     pdb.post_mortem(tb)
-        initial_pop, last_fitness = trainer.run()
+        starting_model, last_fitness = trainer.run()
         print(f"Training with {ns} samples finished with fitness {last_fitness}")
         print("=" * 45)
 
