cf90312739b3380adf8c3a51f7bdc165f6c72c28
Connor Fuhrman <connorfuhrman@email.arizona.edu>
2023-01-07 19:25:27 -0700

    Updates for training

--- container/docker/build.sh
+++ container/docker/build.sh
@@ -3,5 +3,4 @@
 SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
 
 cd ${SCRIPT_DIR}/../../
-find . -type d -name '__pycache__' -exec rm -r {} \;
 sudo docker build -f ${SCRIPT_DIR}/Dockerfile . -t spacetrex/dsrc:latest
